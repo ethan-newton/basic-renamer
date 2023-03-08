23ea3f827c35ff46af38fe03552a713641e5e8e9
ethan-newton <126926188+ethan-newton@users.noreply.github.com>
2023-03-07 20:23:31 -0500

    Update basic-renamer.py

--- basic-renamer.py
+++ basic-renamer.py
@@ -8,10 +8,13 @@ renaming_type = input("What type of renaming do you want to do? Choose one of th
                       "1. All caps\n"
                       "2. All lowercase\n"
                       "3. Capitalize first letter of every word\n"
-                      "4. Replace _ for - or space\n"
-                      "5. Replace - for _ or space\n"
-                      "6. Replace spaces for _ or -\n"
-                      "7. Text in camelCase\n"
+                      "4. Replace underscore for dash\n"
+                      "5. Replace underscore for space\n"
+                      "6. Replace dash for underscore\n"
+                      "7. Replace dash for space\n"
+                      "8. Replace space for underscore\n"
+                      "9. Replace space for dash\n"
+                      "10. Text in camelCase\n"
                       "Enter the number of your choice: ")
 
 # Loop through each file
@@ -26,13 +29,20 @@ for file in files:
         elif renaming_type == "3":
             new_file_name = file.title()
         elif renaming_type == "4":
-            new_file_name = file.replace("_", "-").replace(" ", "-")
+            new_file_name = file.replace("_", "-")
         elif renaming_type == "5":
-            new_file_name = file.replace("-", "_").replace(" ", "_")
+            new_file_name = file.replace("_", " ")
         elif renaming_type == "6":
-            new_file_name = file.replace(" ", "-").replace("_", "-")
+            new_file_name = file.replace("-", "_")
         elif renaming_type == "7":
-            words = file.split(" ")
+            new_file_name = file.replace("-", " ")
+        elif renaming_type == "8":
+            new_file_name = file.replace(" ", "_")
+        elif renaming_type == "9":
+            new_file_name = file.replace(" ", "-")
+        elif renaming_type == "10":
+            # Replace '_' and '-' with spaces before applying camelCase
+            words = file.replace("_", " ").replace("-", " ").split(" ")
             new_words = [words[0].lower()] + [word.capitalize() for word in words[1:]]
             new_file_name = "".join(new_words)
         
